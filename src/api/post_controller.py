4b06b30c1c111fdb116641ba0da514fa3a42798e
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2025-08-04 18:21:47 +0300

    refactor: improve structure of post and user controllers

--- src/api/post_controller.py
+++ src/api/post_controller.py
@@ -5,24 +5,25 @@ from sqlalchemy.orm import Session
 import schemas.posts as posts
 import services.post_service
 from db.database import get_db
-from schemas.responses import APIResponse
+from schemas.responses import SuccessResponse, PaginatedResponse
+from schemas.posts import PostOut
 
 posts_router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
-@posts_router.get("", response_model=APIResponse)
+@posts_router.get("", response_model=PaginatedResponse[PostOut])
 async def get_all_posts(db: Session = Depends(get_db), page: int = 1, limit: int = 10):
     return services.post_service.get_all_posts(db=db, page=page, limit=limit)
 
 
-@posts_router.get("/{post_id}", response_model=APIResponse)
+@posts_router.get("/{post_id}", response_model=SuccessResponse[PostOut])
 async def get_post(post_id: int, db: Session = Depends(get_db)):
     return services.post_service.get_post_by_id(db=db, post_id=post_id)
 
 
 @posts_router.post(
     "",
-    response_model=APIResponse,
+    response_model=SuccessResponse[PostOut],
     status_code=status.HTTP_201_CREATED,
 )
 async def create_post(
@@ -31,7 +32,7 @@ async def create_post(
     return services.post_service.create_post(db=db, post_data=post_data)
 
 
-@posts_router.patch("/{post_id}/title", response_model=APIResponse)
+@posts_router.patch("/{post_id}/title", response_model=SuccessResponse[PostOut])
 async def update_title(
     post_id: int,
     db: Session = Depends(get_db),
@@ -40,7 +41,7 @@ async def update_title(
     return services.post_service.update_title(db=db, post_id=post_id, title=title)
 
 
-@posts_router.patch("/{post_id}/content", response_model=APIResponse)
+@posts_router.patch("/{post_id}/content", response_model=SuccessResponse[PostOut])
 async def update_content(
     post_id: int,
     db: Session = Depends(get_db),
@@ -49,7 +50,7 @@ async def update_content(
     return services.post_service.update_content(db=db, post_id=post_id, content=content)
 
 
-@posts_router.delete("/{post_id}", response_model=APIResponse)
+@posts_router.delete("/{post_id}", response_model=SuccessResponse[str])
 async def delete_post(
     post_id: int,
     db: Session = Depends(get_db),
--- src/api/user_controller.py
+++ src/api/user_controller.py
@@ -3,21 +3,21 @@ from fastapi import APIRouter, Body, Depends, status
 from sqlalchemy.orm import Session
 
 from db.database import get_db
-from schemas.users import UserCreate
+from schemas.users import UserCreate, UserOut
 from services import user_service
-from schemas.responses import APIResponse
+from schemas.responses import SuccessResponse
 
 users_router = APIRouter(prefix="/users", tags=["Users"])
 
 
-@users_router.get("/{user_id}", response_model=APIResponse)
+@users_router.get("/{user_id}", response_model=SuccessResponse[UserOut])
 async def get_user(user_id: int, db: Session = Depends(get_db)):
     return user_service.get_user_by_id(db=db, user_id=user_id)
 
 
 @users_router.post(
     "",
-    response_model=APIResponse,
+    response_model=SuccessResponse[UserOut],
     status_code=status.HTTP_201_CREATED,
 )
 async def create_user(user_data: UserCreate = Body(...), db: Session = Depends(get_db)):
